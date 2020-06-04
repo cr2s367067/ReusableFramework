0693d49339efe3a6e87007e534076d4da1c1e1f1
KH <cr2s367067@gmial.com>
2020-06-04 22:27:48 +0800

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "ReusableFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ReusableFramework",
+            type: .dynamic,
             targets: ["ReusableFramework"]),
     ],
     dependencies: [
